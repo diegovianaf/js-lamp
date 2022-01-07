9abf16898489cfffc741ec3d3113cbf9adfd0643
Diego Viana <92064022+diegovianaf@users.noreply.github.com>
2022-01-06 22:43:30 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let turnOn = document.querySelector('#turnOn')
+let turnOff = document.querySelector('#turnOff')
+let lamp = document.querySelector('#lamp')
+
+function lampOn(){
+  lamp.src = 'img/lamp-on.gif'
+}
+
+function lampOff(){
+  lamp.src = 'img/lamp-off.gif'
+}
+
+turnOn.addEventListener('click', lampOn)
+turnOff.addEventListener('click', lampOff)
